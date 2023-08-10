edf496f12fdb739c21860e779e10ccf0c32179d7
Sumit Khamar <57529963+sumitkhamar2260@users.noreply.github.com>
2023-08-10 18:10:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PostModel = require('./models/PostModel')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://blog-app-three-taupe.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
